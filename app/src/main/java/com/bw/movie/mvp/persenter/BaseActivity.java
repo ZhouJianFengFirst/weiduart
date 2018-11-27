5aa1a7db0f93d966ee95b686b7ccd55c57cb2176
ZhouJianFengFirst <42331894+ZhouJianFengFirst@users.noreply.github.com>
2018-11-27 10:56:50 +0800

    周建峰 框架搭建

---
+++ app/src/main/java/com/bw/movie/mvp/persenter/BaseActivity.java
@@ -0,0 +1,48 @@
+package com.bw.movie.mvp.persenter;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bw.movie.mvp.view.AppDelegate;
+
+
+public abstract class BaseActivity<T extends AppDelegate> extends AppCompatActivity {
+
+    public T delegate;
+
+    public abstract Class<T> getDelegateClass();
+
+    public BaseActivity() {
+        try {
+            delegate = getDelegateClass().newInstance();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        delegate.initContext(this);
+        delegate.creat(getLayoutInflater(), null, savedInstanceState);
+        setContentView(delegate.getRootView());
+        initData();
+        delegate.initData();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        delegate.destory();
+        delegate = null;
+    }
+
+    public void initData(){
+
+    }
+
+
+}
---
+++ app/src/main/java/com/bw/movie/mvp/view/AppDelegate.java
@@ -0,0 +1,150 @@
+package com.bw.movie.mvp.view;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.tapadoo.alerter.Alerter;
+import com.xiangri.dongdong.R;
+import com.xiangri.dongdong.net.BaseObserver;
+import com.xiangri.dongdong.net.RetrofitHelper;
+
+import java.io.IOException;
+import java.util.Map;
+
+import okhttp3.ResponseBody;
+
+public abstract class AppDelegate implements IDelegate {
+
+    private Context mContext;
+    private SparseArray<View> views = new SparseArray<>();
+    private View rootView;
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void creat(LayoutInflater inflater, ViewGroup viewGroup, Bundle bundle) {
+        rootView = inflater.inflate(getLayoutId(), null);
+    }
+
+    protected abstract int getLayoutId();
+
+    @Override
+    public View getRootView() {
+        return rootView;
+    }
+
+    @Override
+    public void initContext(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public void getString(String url, final int type, Map<String, String> map) {
+
+        BaseObserver ob = new BaseObserver<ResponseBody>() {
+
+            @Override
+            public void onNext(ResponseBody responseBody) {
+                if (responseBody != null){
+                    String data = null;
+                    try {
+                        data = responseBody.string();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    successString(data,type);
+                }else{
+                    failString("请求错误");
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                failString(e.getMessage());
+            }
+        };
+        RetrofitHelper.getInstens().doGet(url, map,ob);
+    }
+
+
+    @Override
+    public void postString(String url, final int type, Map<String,String> map) {
+
+        BaseObserver ob = new BaseObserver<ResponseBody>() {
+
+            @Override
+            public void onNext(ResponseBody responseBody) {
+                if (responseBody != null){
+                    String data = null;
+                    try {
+                        data = responseBody.string();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    successString(data,type);
+                }else{
+                    failString("请求错误");
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                failString(e.getMessage());
+            }
+        };
+        RetrofitHelper.getInstens().doPost(url, map,ob);
+
+    }
+
+    public void successString(String data, int type) {
+
+    }
+
+    public void failString(String msg) {
+
+    }
+
+    public void toast(String msg) {
+        Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public void toast(String title,String msg,int s){
+        Alerter.create(((AppCompatActivity)mContext)).setBackgroundColor(R.color.colorPrimary)
+                .setText(msg)
+                .setTitle(title)
+                .setDuration(s)
+                .show();
+    }
+
+    public <T extends View> T getView(int viewId) {
+        T view = (T) views.get(viewId);
+        if (view == null) {
+            view = rootView.findViewById(viewId);
+            views.put(viewId, view);
+        }
+        return view;
+    }
+
+    public void setClick(View.OnClickListener listener, int... ids) {
+
+        if (ids == null) {
+            return;
+        }
+        for (int id : ids) {
+            getView(id).setOnClickListener(listener);
+        }
+    }
+
+    public void destory(){
+        rootView = null;
+    }
+}
