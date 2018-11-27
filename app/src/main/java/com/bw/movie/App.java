b27410a84f105dd4df9e71d0e9db52db6ad50c89
M <2946268162@qq.com>
2018-11-27 19:38:31 +0800

    user head

--- app/src/main/java/com/bw/movie/App.java
+++ app/src/main/java/com/bw/movie/App.java
@@ -3,6 +3,8 @@ package com.bw.movie;
 import android.app.Application;
 
 import com.bw.movie.net.HttpHelper;
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 /**
  * 作者：zhoujianfeng
  * 时间：2018/11/27
@@ -13,5 +15,7 @@ public    class App extends Application {
     public void onCreate() {
         super.onCreate();
         HttpHelper.getInstens().init("http://172.17.8.100/",this);
+        //初始化fresco图片控件
+        Fresco.initialize(this);
     }
 }
