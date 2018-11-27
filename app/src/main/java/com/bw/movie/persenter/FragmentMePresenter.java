d01817fa0c88fd05edd607bdbf2843449965c126
M <2946268162@qq.com>
2018-11-27 20:28:36 +0800

    me

--- app/src/main/java/com/bw/movie/persenter/FragmentMePresenter.java
+++ app/src/main/java/com/bw/movie/persenter/FragmentMePresenter.java
@@ -2,6 +2,7 @@ package com.bw.movie.persenter;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,9 +12,14 @@ import com.facebook.drawee.view.SimpleDraweeView;
 
 public    class FragmentMePresenter extends AppDelegate implements View.OnClickListener {
     private Context context;
-    private SimpleDraweeView sdv_head;
-    private SimpleDraweeView sdv_inform;
-    private TextView tv_nickname;
+    private SimpleDraweeView me_sdv_head;
+    private SimpleDraweeView me_sdv_inform;
+    private TextView me_tv_nickname;
+    private LinearLayout me_liner_message;
+    private LinearLayout me_Liner_attention;
+    private LinearLayout me_Liner_history;
+    private LinearLayout me_Liner_opinion;
+    private LinearLayout me_Liner_newest;
 
     @Override
     protected int getLayoutId() {
@@ -38,25 +44,51 @@ public    class FragmentMePresenter extends AppDelegate implements View.OnClickL
     //初始化数据方法
     private void initwidget() {
         //获取控件强转提上去
-        sdv_head=(SimpleDraweeView)getView(R.id.sdv_head);
-        sdv_inform=(SimpleDraweeView)getView(R.id.sdv_inform);
-        tv_nickname=(TextView)getView(R.id.tv_nickname);
+        me_sdv_head=(SimpleDraweeView)getView(R.id.me_sdv_head);
+        me_sdv_inform=(SimpleDraweeView)getView(R.id.me_sdv_inform);
+        me_tv_nickname=(TextView)getView(R.id.me_tv_nickname);
+        me_liner_message=(LinearLayout)getView(R.id.me_liner_message);
+        me_Liner_attention=(LinearLayout)getView(R.id.me_Liner_attention);
+        me_Liner_history=(LinearLayout)getView(R.id.me_Liner_history);
+        me_Liner_opinion=(LinearLayout)getView(R.id.me_Liner_opinion);
+        me_Liner_newest=(LinearLayout)getView(R.id.me_Liner_newest);
         //点击事件
-        sdv_head.setOnClickListener(this);
-        sdv_inform.setOnClickListener(this);
+        me_sdv_head.setOnClickListener(this);
+        me_sdv_inform.setOnClickListener(this);
+        me_liner_message.setOnClickListener(this);
+        me_Liner_attention.setOnClickListener(this);
+        me_Liner_history.setOnClickListener(this);
+        me_Liner_opinion.setOnClickListener(this);
+        me_Liner_newest.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         //选择点击事件
         switch (view.getId()){
-            case R.id.sdv_head:
+            case R.id.me_sdv_head:
                 //吐司这是头像
                 Toast.makeText(context,"这是头像",Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.sdv_inform:
+            case R.id.me_sdv_inform:
                 //吐司这是通知
                 Toast.makeText(context,"这是通知",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.me_liner_message:
+                //吐司这是我的信息
+                Toast.makeText(context,"这是我的信息",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.me_Liner_attention:
+                //吐司这是我的关注
+                Toast.makeText(context,"这是我的关注信息",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.me_Liner_history:
+                //吐司这是我的购票记录
+                Toast.makeText(context,"这是我的购票记录",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.me_Liner_opinion:
+                //吐司这是我的意见反馈
+                Toast.makeText(context,"这是我的意见反馈",Toast.LENGTH_SHORT).show();
                 break;
         }
     }
