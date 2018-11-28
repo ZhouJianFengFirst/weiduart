7bf53f4b2129b6ab37d6c8287378f5a36e0b36dc
ZhouJianFengFirst <42331894+ZhouJianFengFirst@users.noreply.github.com>
2018-11-28 14:59:50 +0800

    add

---
+++ app/src/main/java/com/bw/movie/adapter/BasePageChangeListener.java
@@ -0,0 +1,18 @@
+package com.bw.movie.adapter;
+
+import android.support.v4.view.ViewPager;
+
+public abstract class BasePageChangeListener implements ViewPager.OnPageChangeListener {
+    @Override
+    public void onPageScrolled(int i, float v, int i1) {
+
+    }
+
+    @Override
+    public abstract void onPageSelected(int i);
+
+    @Override
+    public void onPageScrollStateChanged(int i) {
+
+    }
+}
