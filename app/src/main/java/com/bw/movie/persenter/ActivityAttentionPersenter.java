a9bcb832084415235ea552e901d8321296d20900
M <2946268162@qq.com>
2018-11-28 15:13:16 +0800

    mfy

--- app/src/main/java/com/bw/movie/persenter/ActivityAttentionPersenter.java
+++ app/src/main/java/com/bw/movie/persenter/ActivityAttentionPersenter.java
@@ -1,9 +1,16 @@
 package com.bw.movie.persenter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.TextView;
 
 import com.bw.movie.R;
+import com.bw.movie.activitys.ActivityAttention;
 import com.bw.movie.mvp.view.AppDelegate;
+import com.example.xlistviewlib.XListView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * 作者：mafuyan
@@ -12,8 +19,12 @@ import com.bw.movie.mvp.view.AppDelegate;
  * */
 
 //继承APPDelegate
-public class ActivityAttentionPersenter extends AppDelegate{
+public class ActivityAttentionPersenter extends AppDelegate implements View.OnClickListener {
     private Context context;
+    private TextView attention_tv_film;
+    private TextView attention_tv_cinema;
+    private XListView attention_xlv;
+    private CircleImageView attention_cv_leftreturn;
 
     @Override
     protected int getLayoutId() {
@@ -39,6 +50,51 @@ public class ActivityAttentionPersenter extends AppDelegate{
     //初始化控件方法
     private void initwidget() {
         //获取控件强转提上去
+        attention_tv_film=(TextView)getView(R.id.attention_tv_film);
+        attention_tv_cinema=(TextView)getView(R.id.attention_tv_cinema);
+        attention_xlv=(XListView)getView(R.id.attention_xlv);
+        attention_cv_leftreturn=(CircleImageView)getView(R.id.attention_cv_leftreturn);
 
+        //点击事件
+        attention_tv_film.setOnClickListener(this);
+        attention_tv_cinema.setOnClickListener(this);
+        attention_cv_leftreturn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        //选择点击事件
+        switch (view.getId()){
+            case R.id.attention_tv_film:
+                //吐司
+//                toast(context,"电影");
+                //给控件重新赋值 给背景改变 设置背景resource
+                attention_tv_film.setBackgroundResource(R.drawable.purplechange);
+                //字体变颜色黑色
+                attention_tv_film.setTextColor(Color.WHITE);
+                //在给本空间设置背景和字体颜色
+                //给控件重新赋值 给背景改变 设置背景resource
+                attention_tv_cinema.setBackgroundResource(R.drawable.square_gray);
+                //字体变颜色黑色
+                attention_tv_cinema.setTextColor(Color.BLACK);
+                break;
+            case R.id.attention_tv_cinema:
+                //吐司
+//                toast(context,"影院");
+                //给控件重新赋值 给背景改变 设置背景resource
+                attention_tv_film.setBackgroundResource(R.drawable.square_gray);
+                //字体变颜色黑色
+                attention_tv_film.setTextColor(Color.BLACK);
+                //在给本空间设置背景和字体颜色
+                //给控件重新赋值 给背景改变 设置背景resource
+                attention_tv_cinema.setBackgroundResource(R.drawable.purplechange);
+                //字体变颜色黑色
+                attention_tv_cinema.setTextColor(Color.WHITE);
+                break;
+            case R.id.attention_cv_leftreturn:
+                //销毁强转上下文页面
+                ((ActivityAttention)context).finish();
+                break;
+        }
     }
 }
