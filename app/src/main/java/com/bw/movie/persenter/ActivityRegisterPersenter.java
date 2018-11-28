0d887482b5b03ac3d2a2b3969a438ab8006f5946
徐家辉 <43642781+xujiahuiz@users.noreply.github.com>
2018-11-28 14:53:26 +0800

    xjh登录注册UI

--- app/src/main/java/com/bw/movie/persenter/ActivityRegisterPersenter.java
+++ app/src/main/java/com/bw/movie/persenter/ActivityRegisterPersenter.java
@@ -2,7 +2,11 @@ package com.bw.movie.persenter;
 
 import com.bw.movie.R;
 import com.bw.movie.mvp.view.AppDelegate;
-
+/**
+ * 作者：xujiahui
+ * 时间：2018/11/28
+ * 作用：ActivityBeginPersenter(注册页)
+ */
 public class ActivityRegisterPersenter extends AppDelegate {
     @Override
     protected int getLayoutId() {
